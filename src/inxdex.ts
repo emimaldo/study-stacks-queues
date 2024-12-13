91b5b247464cadf30f39a3b640393044c90d45e2
emimaldo <emimaldo@gmail.com>
2024-12-13 09:14:17 -0300

    Add the queue example execution

--- src/inxdex.ts
+++ src/inxdex.ts
@@ -14,3 +14,19 @@ console.log(stack.size()); // Imprime 1 (sólo queda 1 elemento en la pila)
 
 stack.clear();             // Vacía la pila
 console.log(stack.isEmpty()); // Imprime true (la pila está vacía)
+
+const queue = new Queue<number>();
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+
+console.log(queue.peek());  // Imprime 10 (el primer elemento)
+
+console.log(queue.dequeue());  // Imprime 10 (y lo quita de la cola)
+console.log(queue.dequeue());  // Imprime 20
+console.log(queue.size());     // Imprime 1 (sólo queda 1 elemento en la cola)
+
+queue.clear();                 // Vacía la cola
+console.log(queue.isEmpty());  // Imprime true (la cola está vacía)
+
